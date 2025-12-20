55cc47a36d28abf86e40dc230d220c7480205bfe
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2025-12-20 01:32:32 -0500

    feat(CORS): agregar configuración de CORS para permitir solicitudes desde localhost

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import express from "express";
+import cors from "cors";
 import { userRoutes } from "./interface/routes/userRoutes";
 import { errorHandler } from "./interface/middlewares/errorHandler";
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 
 app.use(express.json());
 app.use("/api", userRoutes);
