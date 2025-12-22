6049c743535a41f363d1f82c05be02ad7f45124b
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2025-12-22 00:13:45 -0500

    feat(Redis): implementar caché de ubicaciones con Redis

--- src/interface/routes/locationRoutes.ts
+++ src/interface/routes/locationRoutes.ts
@@ -3,11 +3,13 @@ import { PostgresLocationRepository } from "../../infrastructure/repositories/Po
 import { GetLocations } from "../../use-cases/location/GetLocations";
 import { LocationController } from "../controllers/LocationController";
 import { authenticateToken } from "../middlewares/authenticateToken";
+import { RedisLocationCacheRepository } from '../../infrastructure/cahe/RedisLocationCacheRepository';
 
 const router = Router();
 
 const locationRepository = new PostgresLocationRepository();
-const getLocations = new GetLocations(locationRepository);
+const redisLocationCacheRepository = new RedisLocationCacheRepository();
+const getLocations = new GetLocations(locationRepository, redisLocationCacheRepository);
 const locationController = new LocationController(getLocations);
 
 router.get("/",authenticateToken ,async (req, res, next) =>
